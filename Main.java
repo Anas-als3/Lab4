78b1819db6452dfa884b609ab4896adedc82c78e
Anas-als3 <anas-alsubaie@hotmail.com>
2026-04-16 11:51:59 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,11 +2,20 @@ package Shapes1;
 
 public class Main {
     public static void main(String[]args){
-        Circle c= new Circle(3);
+       Circle c= new Circle();
+        //Out Put The Area and Circumference of the circle and Test if both works
+        c.setRadius(5);
         System.out.println(c);
-        Rectangle r= new Rectangle(3,5);
+
+        //Out Put The Area and Perimeter of a Rectangle and Test if both works
+        Rectangle r= new Rectangle();
+        r.setHeight(3);
+        r.setWidth(5);
         System.out.println(r);
-        Triangle t= new Triangle(10,10);
+        //Out Put The Area and Perimeter of a Triangle and Test if both works
+        Triangle t= new Triangle();
+        t.setHeight(10);
+        t.setBase(10);
         System.out.println(t);
     }
 
